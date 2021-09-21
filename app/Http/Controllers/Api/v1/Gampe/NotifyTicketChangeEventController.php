6be8dd09483ff2971bac9470714a3e049ce4be43
Roger Rojas <rrojas@indracompany.com>
2021-09-21 18:08:12 -0500

    feat(rf-019): construccion del ws para consumir kafka

---
+++ app/Http/Controllers/Api/v1/Gampe/NotifyTicketChangeEventController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Http\Controllers\Api\v1\Gampe;
+
+use App\Contracts\Gampe\NotifyTicketRepository;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+final class NotifyTicketChangeEventController extends Controller
+{
+    protected $repository;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(NotifyTicketRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function __invoke(Request $request)
+    {
+        $body = $request->all();
+        $data = $this->repository->notifyTicketAttributeValueChangeEvent(
+            $body
+        );
+        return response()->json($data);
+    }
+}
